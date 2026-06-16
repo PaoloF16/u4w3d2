3ea60e3007fe165a57eeca74ab54a03f3e7b21dc
PaoloF16 <paolo.cafis@gmail.com>
2026-06-16 14:20:40 +0200

    start repo

---
+++ src/main/java/paolof16/Application.java
@@ -0,0 +1,11 @@
+package paolof16;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u412pu");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/paolof16/ApplicationTest.java
@@ -0,0 +1,13 @@
+package paolof16;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
